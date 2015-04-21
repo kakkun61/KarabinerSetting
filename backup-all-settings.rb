7e7d63748287bc3b0ab034e2f52539ff6fba50af
Kazuki Okamoto <okamoto-k@klab.com>
2015-04-21 13:16:54 +0900

    add seil back up script

--- backup-all-settings.rb
+++ backup-all-settings.rb
@@ -6,7 +6,7 @@ def mkdir(dir)
     Dir.mkdir(dir) unless Dir.exists?(dir)
 end
 
-def backup_sh
+def karabiner_backup_sh
     karabiner = '/Applications/Karabiner.app/Contents/Library/bin/karabiner'
 
     selected = `#{karabiner} selected`
@@ -26,18 +26,34 @@ def backup_sh
     `#{karabiner} select #{selected}`
 end
 
-def backup_xml
+def karabiner_backup_xml
     mkdir("#{$work_dir}/xml")
     `cp "$HOME/Library/Application Support/Karabiner/private.xml" "#{$work_dir}/xml/"`
 end
 
-def backup_plist
+def karabiner_backup_plist
     mkdir("#{$work_dir}/plist")
     Dir.glob("#{ENV['HOME']}/Library/Preferences/org.pqrs.Karabiner*.plist").each { |file|
         `cp "#{file}" "#{$work_dir}/plist/"`
     }
 end
 
-backup_sh
-backup_xml
-backup_plist
+def seil_backup_sh
+    seil = '/Applications/Seil.app/Contents/Library/bin/seil'
+
+    mkdir("#{$work_dir}/sh")
+    `#{seil} export > "#{$work_dir}/sh/seil.sh"`
+end
+
+
+def karabiner_backup_plist
+    mkdir("#{$work_dir}/plist")
+    `cp "#{ENV['HOME']}/Library/Preferences/org.pqrs.Seil.plist" "#{$work_dir}/plist/"`
+end
+
+karabiner_backup_sh
+karabiner_backup_xml
+karabiner_backup_plist
+
+seil_backup_sh
+karabiner_backup_plist
